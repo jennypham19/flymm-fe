a5c5822ca8e6232e34184f8aa3ae3fb122a2d0a3
ngocanh <ngocanh.pham19042001@gmail.com>
2026-03-30 00:22:23 +0700

    route

---
+++ src/views/LandingPage/Home/index.tsx
@@ -0,0 +1,9 @@
+import Page from "@/components/Page";
+
+const Home = () => {
+    return (
+        <Page title='Flymm - Trang chủ'>Flymm - Trang chủ</Page>
+    );
+}
+
+export default Home;
---
+++ src/views/LandingPage/Service/index.tsx
@@ -0,0 +1,7 @@
+import Page from '@/components/Page';
+
+const Service = () => {
+  return <Page title='Flymm - Dịch vụ'>Flymm - Dịch vụ</Page>;
+};
+
+export default Service;
